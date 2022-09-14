de1f036b1eb67f2c8539c93a51c5addff3028fa8
Michael N <37873018+webjestic@users.noreply.github.com>
2022-09-14 11:15:38 -0400

    chore: pubspec updated and ci implemented (#4)
    * chore: pubspec updated and ci implemented
    
    * fix: updated workling directory
    
    Co-authored-by: Michael Newlon <michael.newlon@tn.gov>

--- lib/app/resources/app_strings.dart
+++ lib/app/resources/app_strings.dart
@@ -4,4 +4,5 @@
 class AppStrings {
   final String appTitle = 'obotix';
   final String myHomePage = 'Flutter Demo Home Page';
+  final String aTestString = 'another line that goes well beyond the lenght of 80 characters and reaches 124 if i can';
 }
